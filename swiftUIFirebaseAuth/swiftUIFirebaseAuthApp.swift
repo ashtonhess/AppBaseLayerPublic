03668f38fe5b4b96463acbcccbd79ee84beb8f07
ashtonhess <ashton.hess@yahoo.com>
2021-12-01 19:29:43 -0600

    Initial Commit

---
+++ swiftUIFirebaseAuth/swiftUIFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIFirebaseAuthApp.swift
+//  swiftUIFirebaseAuth
+//
+//  Created by Ashton Hess on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
